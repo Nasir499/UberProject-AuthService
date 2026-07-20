fde215fd1d18372f71bbc8ba43122fa8bd11fb6a
Nasir <nasirhossain1070@gmail.com>
2026-07-20 22:30:56 +0530

    added userdetails class

---
+++ src/main/java/com/example/uberprojectauthservice/helpers/AuthPassengerDetails.java
@@ -0,0 +1,57 @@
+package com.example.uberprojectauthservice.helpers;
+
+import com.example.uberprojectauthservice.model.Passenger;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class AuthPassengerDetails extends Passenger implements UserDetails {
+
+    private String username;//email/name/id
+
+    private String password;
+
+    public AuthPassengerDetails(Passenger passenger) {
+        this.username = passenger.getEmail();
+        this.password = passenger.getPassword();
+    }
+
+    /**
+     * Returns the authorities granted to the user. Cannot return <code>null</code>.
+     *
+     * @return the authorities, sorted by natural key (never <code>null</code>)
+     */
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    /**
+     * Returns the username used to authenticate the user. Cannot return
+     * <code>null</code>.
+     *
+     * @return the username (never <code>null</code>)
+     */
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/example/uberprojectauthservice/services/UserDetailsServiceImpl.java
@@ -0,0 +1,43 @@
+package com.example.uberprojectauthservice.services;
+
+import com.example.uberprojectauthservice.helpers.AuthPassengerDetails;
+import com.example.uberprojectauthservice.model.Passenger;
+import com.example.uberprojectauthservice.repository.PassengerRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final PassengerRepository passengerRepository;
+
+    public UserDetailsServiceImpl(PassengerRepository passengerRepository) {
+        this.passengerRepository = passengerRepository;
+    }
+
+    /**
+     * Locates the user based on the username. In the actual implementation, the search
+     * may possibly be case sensitive, or case insensitive depending on how the
+     * implementation instance is configured. In this case, the <code>UserDetails</code>
+     * object that comes back may have a username that is of a different case than what
+     * was actually requested..
+     *
+     * @param email the username identifying the user whose data is required.
+     * @return a fully populated user record (never <code>null</code>)
+     * @throws UsernameNotFoundException if the user could not be found or the user has no
+     *                                   GrantedAuthority
+     */
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<Passenger> passenger = passengerRepository.findPassengerByEmail(email);
+        if(passenger.isPresent()){
+            return new AuthPassengerDetails(passenger.get());
+        }else{
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+    }
+}
